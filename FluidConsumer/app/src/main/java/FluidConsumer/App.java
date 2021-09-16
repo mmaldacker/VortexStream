607db2219336bf3403df6b851b0152f94cb4e6e0
Max <mmaldacker@gmail.com>
2021-09-16 11:03:23 +0200

    use javafx fxml

--- FluidConsumer/app/src/main/java/FluidConsumer/App.java
+++ FluidConsumer/app/src/main/java/FluidConsumer/App.java
@@ -1,78 +1,22 @@
 package FluidConsumer;
 
-import VortexStream.Messages;
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.event.ActionEvent;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Slider;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+import javafx.fxml.FXMLLoader;
 
 public class App extends Application  {
-
-    private Player player;
-    private Queue queue;
-
     public App() {
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        var root = new GridPane();
-        root.setHgap(8);
-        root.setVgap(8);
-        root.setPadding(new Insets(5));
+        var loader = new FXMLLoader(getClass().getClassLoader().getResource("app.fxml"));
+        loader.setController(new Controller());
+        Parent root =  loader.load();
 
-        queue = new Queue();
-
-        player = new Player(100, 100);
-        player.start();
-        root.add(player.imageView, 0, 0, 5, 1);
-
-        var slider = new Slider(0, 100, 0);
-        slider.setBlockIncrement(1);
-        slider.setMajorTickUnit(1);
-        slider.setMinorTickCount(0);
-        slider.setSnapToTicks(true);
-        slider.setPrefWidth(200);
-        root.add(slider, 1, 1);
-
-        var numFrameLabel = new Label();
-        numFrameLabel.textProperty().bind(Bindings.format("Num frames: %.0f", slider.valueProperty()));
-        root.add(numFrameLabel, 0, 1);
-
-        var requestBtn = new Button("Request");
-        requestBtn.setOnAction((ActionEvent event) -> {
-            try {
-                queue.RequestFrames((int)slider.getValue(), (Messages.Frame frame) -> {
-                    player.addFrame(frame);
-                });
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        GridPane.setHalignment(requestBtn, HPos.RIGHT);
-        root.add(requestBtn, 2, 1);
-
-        var progressLabel = new Label("Loaded: ");
-        root.add(progressLabel, 3, 1);
-
-        var progress = new ProgressBar(0);
-        progress.setPrefWidth(150);
-        root.add(progress, 4, 1);
-
-        Scene scene = new Scene(root, 1000, 1000);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
